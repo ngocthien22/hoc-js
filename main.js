9c9d394ab5a8286df7430cfc14f08f0f5c67fe19
ngocthien22 <ngocthien1098@gmail.com>
2022-03-02 16:14:39 +0700

    first

---
+++ main.js
@@ -0,0 +1,18 @@
+var string = "test";
+
+//number
+var so = 2;
+var arr=[1,2,3];
+//ob
+var ob ={
+    "key1": 3,
+    "key2":[1,2,3],
+    "key":"sssss"
+}
+function test(arr) {
+    console.log(this.string)
+    this.arr.forEach( x => {
+        console.log(x)
+    });
+}
+const
